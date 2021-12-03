efeae2bd249d73d127d2cd3ca8624dd374a7fd96
WrathValor <86674874+WrathValor@users.noreply.github.com>
2021-12-03 15:50:50 +0000

    Attempt Fix

--- epsilion.lua
+++ epsilion.lua
@@ -53,7 +53,7 @@ game:GetService("RunService"):Connect(function()
         end
     end
     
-    if shared.pickup then
+   -- if shared.pickup then
         while shared.pickup do
             for i,v in pairs(game:GetService("Workspace").MouseIgnore:GetDescendants()) do
     		if v:IsA("ClickDetector") then
@@ -62,5 +62,5 @@ game:GetService("RunService"):Connect(function()
             end
 	    wait()
         end
-    end
+ --   end
 end)
